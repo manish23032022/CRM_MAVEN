5bc663533807749de74dbd1efaf8187e911fb6c9
User <User@DESKTOP-PCQAT3M>
2022-03-24 21:46:38 +0530

    second commit

--- src/test/java/Practice/Demo.java
+++ src/test/java/Practice/Demo.java
@@ -7,4 +7,13 @@ public class Demo {
 public void createOrg() {
 	System.out.println("create org succesfull");
 }
+
+@Test
+public void modifyOrg() {
+	System.out.println("modify org succesfull");
+}
+@Test
+public void deleteOrg() {
+	System.out.println("delete org succesfull");
+}
 }
